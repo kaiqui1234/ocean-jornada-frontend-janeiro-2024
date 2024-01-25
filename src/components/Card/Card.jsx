ddccb97e0ad87d03fe1296a98e883457bd4ac0a8
kaiqui1234 <150301564+kaiqui1234@users.noreply.github.com>
2024-01-25 15:00:45 +0000

    Extração do componente <Card>

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,14 @@
+function Card(){
+    return (
+        <>
+          <div className='card'>
+            <h2>Rick Sanchez</h2>
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"></img>
+            
+          </div>
+         
+        </>
+      )   
+}
+
+export default Card
