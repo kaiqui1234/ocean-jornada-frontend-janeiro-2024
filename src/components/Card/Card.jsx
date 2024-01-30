d9afd3f03a7338942147e39d31e8a510d215a938
kaiqui1234 <150301564+kaiqui1234@users.noreply.github.com>
2024-01-30 13:36:55 +0000

    configuração para o card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,22 @@
-function Card(){
+function Card(props){
+  //console.log(props)
+
+   const item = props.item
+
+   //console.log(item)
+   
     return (
         <>
           <div className='card'>
-            <h2>Rick Sanchez</h2>
+            <h2>{item.name}</h2>
+
             <div className="tags">
             <div className="tag">Status: Vivo</div>
               <div className="tag">Espécie: Humana</div>
             <div className="tag">Origem:Terra C-137</div>
             </div>
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"></img>
+
+            <img src={item.image}></img>
             
           </div>
          
