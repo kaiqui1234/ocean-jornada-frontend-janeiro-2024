dd481a1ab1c5d60d7a61b751ef39de03b44d7abd
kaiqui1234 <150301564+kaiqui1234@users.noreply.github.com>
2024-01-30 14:11:46 +0000

    estilização da lista de cards

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,29 @@ import './App.css'
 import Card from './components/Card/Card'
 function App(){
   const item1= {
-    name:'Morty Smith',
+    name:'Rick Sanchez',
     image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
   }
   const item2= {
-    name:'Rick Sanchez',
+    name: 'Morty Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
   }
   const item3= {
     name:'Sam Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
+  const item4= {
+    name:'Beth Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  }
   return (
     <>
+       <div className="cards">
       <Card item={item1} />
-      <Card item={item1}/>
-     
+      <Card item={item2} />
+      <Card item={item3}/>
+      <Card item={item4}/>
+      </div>
     </>
   )
 }
