7cf3c7a21a4a64ac5476a95e1f2bb6955116a5fd
kaiqui1234 <150301564+kaiqui1234@users.noreply.github.com>
2024-01-30 14:38:43 +0000

    implementação do app.

--- src/App.jsx
+++ src/App.jsx
@@ -11,20 +11,24 @@ function App(){
     image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
   }
   const item3= {
-    name:'Sam Smith',
+    name:'Summer Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
   const item4= {
     name:'Beth Smith',
     image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
   }
+
+const itens = [item1, item2, item3, item4]
+
   return (
     <>
        <div className="cards">
-      <Card item={item1} />
+      {/* <Card item={item1} />
       <Card item={item2} />
       <Card item={item3}/>
-      <Card item={item4}/>
+      <Card item={item4}/> */}
+      {itens.map(item => <Card item={item} />)}
       </div>
     </>
   )
