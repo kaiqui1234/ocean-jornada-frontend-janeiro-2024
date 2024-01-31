cb80fff065e19467aee31a7cd5e10d927976bc8f
kaiqui1234 <150301564+kaiqui1234@users.noreply.github.com>
2024-01-31 15:13:10 +0000

    finalização do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
  function App(){
   //Carregamento de dados direto via código
   
-  const item1= {
-    name:'Rick Sanchez',
-    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
-  }
-  const item2= {
-    name: 'Morty Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
-  }
-  const item3= {
-    name:'Summer Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
-  }
-  const item4= {
-    name:'Beth Smith',
-    image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
-  }
+  // const item1= {
+  //   name:'Rick Sanchez',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  // }
+  // const item2= {
+  //   name: 'Morty Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  // }
+  // const item3= {
+  //   name:'Summer Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  // }
+  // const item4= {
+  //   name:'Beth Smith',
+  //   image: 'https://rickandmortyapi.com/api/character/avatar/4.jpeg'
+  // }
 
-const itens = [item1, item2, item3, item4]
+//const itens = [item1, item2, item3, item4]
+
+
+const [itens, setItens] = useState([])
 
 //Carregamento de dados via API
 async function carregarDadosApi(){
@@ -34,8 +38,20 @@ const body = await response.json()
 console.log(body)
 
 const results = body.results;
+
+console.log(results)
+
+setItens(results)
+
 }
 carregarDadosApi()
+
+useEffect(function(){
+
+  carregarDadosApi()
+},[])
+
+
   return (
     <>
        <div className="cards">
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,13 +9,13 @@ function Card(props){
         <>
           <div className='card'>
             <h2>{item.name}</h2>
-
+            <div>
             <div className="tags">
             <div className="tag">Status: Vivo</div>
             <div className="tag">Espécie: Humana</div>
             <div className="tag">Origem:Terra C-137</div>
             </div>
-
+              </div>
             <img src={item.image}></img>
             
           </div>
